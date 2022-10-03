b443edcc92daa0bdd1eeb996728355d53b0d0a7d
siropregno <89697441+siropregno@users.noreply.github.com>
2022-10-03 11:47:47 -0300

    Add files via upload

--- preentrega1pregno.js
+++ preentrega1pregno.js
@@ -16,24 +16,15 @@ do{
 switch(item)
 {
     case "armor": totalWeight = totalWeight+18; armor = armor + 227; 
-    console.log("You've equipped an armor.");
-    console.log("Current armor: "+armor);
-    console.log("Current damage: "+damage);
-    console.log("Weight: "+totalWeight+"/160"); break;
+    alert("You've equipped an armor.\n Current armor: "+armor+". Current damage: "+damage+"- Weight: "+totalWeight+"/160"); break;
 
     case "sword": totalWeight = totalWeight+8; damage = damage + 156; 
-    console.log("You've equipped a sword.");
-    console.log("Current armor: "+armor);
-    console.log("Current damage: "+damage);
-    console.log("Weight: "+totalWeight+"/160"); break;
+    alert("You've equipped a sword.\n Current armor: "+armor+". Current damage: "+damage+"- Weight: "+totalWeight+"/160"); break;
     
     case "shield": totalWeight = totalWeight+20; armor = armor + 227;  
-    console.log("You've equipped a shield.");
-    console.log("Current armor: "+armor);
-    console.log("Current damage: "+damage);
-    console.log("Weight: "+totalWeight+"/160"); break;
+    alert("You've equipped a shield.\n Current armor: "+armor+". Current damage: "+damage+"- Weight: "+totalWeight+"/160"); break;
 
-    default: console.log("You don't have that item");
+    default: alert("You don't have that item");
 }
 
 item = (prompt("Equip another item or type 'exit' to close the inventory"))
